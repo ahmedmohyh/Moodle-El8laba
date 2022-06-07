7944e3fff0f1f0e14595e8a389451c37052d6508
Ahmed Mousa <Ahmed.Mousa98@eng-st.cu.edu.eg>
2022-06-07 19:21:53 +0200

    adding the annotation for the rest api

--- src/main/java/com/startup/MoodleEl8laba/controllers/Course.java
+++ src/main/java/com/startup/MoodleEl8laba/controllers/Course.java
@@ -1,4 +1,10 @@
 package com.startup.MoodleEl8laba.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/courses")
 public class Course {
+
 }
